c57277d554df89554398ba66537270e79b3cac60
Brian Dang <pandabrian1@gmail.com>
2023-11-30 20:41:35 -0700

    Enemy Movement and Mouse Input
    The enemy now moves with a smooth motion rather than teleporting. The tiles now detect mouse clicks on grass tiles and not on the path tiles.

--- Project RealmDefense/Assets/Prefabs/Enemy/EnemyMover.cs
+++ Project RealmDefense/Assets/Prefabs/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField][Range(0f, 5f)] float enemySpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPos = transform.position;
+            Vector3 endPos = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPos);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * enemySpeed;
+                transform.position = Vector3.Lerp(startPos, endPos, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
